e4f973bf892dde414cb9b46cf129329745816006
Micaela Besa <micaela.besasso@hotmail.com>
2026-03-22 19:53:51 +0100

    añado cv y certificado descargable

--- src/app/components/word-carousel.component/word-carousel.component.ts
+++ src/app/components/word-carousel.component/word-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-word-carousel',
@@ -7,6 +7,26 @@ import { Component, ChangeDetectionStrategy, input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WordCarouselComponent {
-  words = input<string[]>(['inspiration', 'innovation', 'creativity']);
-  wordInterval = input<number>(3000);
+  words = input<string[]>(['DESARROLLADORA_FULL_STACK', 'DISPONIBLE_2026']);
+  wordInterval = input<number>(6000);
+  currentIndex = signal(0);
+
+  constructor() {
+    setInterval(() => {
+      this.currentIndex.update(i => (i + 1) % this.words().length);
+    }, this.wordInterval());
+  }
+
+  isWordExiting(index: number): boolean {
+    return index === this.getPreviousIndex();
+  }
+
+  isWordEntering(index: number): boolean {
+    return index === this.currentIndex();
+  }
+
+  private getPreviousIndex(): number {
+    const prevIndex = this.currentIndex() - 1;
+    return prevIndex < 0 ? this.words().length - 1 : prevIndex;
+  }
 }
